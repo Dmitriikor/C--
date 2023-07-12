d5cda40e13a67b2f32763bb850c34ac95aef65f4
DK <DmitriiKor@gmail.com>
2023-07-12 15:20:48 +0300

    modified:   .vscode/tasks.json 	renamed:    build/.cmake/api/v1/reply/index-2023-07-12T09-30-38-0762.json -> build/.cmake/api/v1/reply/index-2023-07-12T12-20-29-0139.json 	modified:   build/CMakeFiles/MAIN.out.dir/main.cpp.o 	modified:   build/MAIN.out 	modified:   build/Testing/20230712-0818/Test.xml 	modified:   build/Testing/Temporary/LastTest_20230712-0818.log 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -35,16 +35,36 @@ int main(int argc, char const *argv[])
 
     // Read a line of input from the user and store it in the beta array
     std::cin.getline(beta, 100);
-    
+
     // Iterate over the elements of the beta array and output each character
     for (size_t i = 0; i < 100; i++)
     {
         std::cout << beta[i];
     }
+
+        // Output a newline character
+    std::cout << "\n\n\n\n";
+
+    //TODO add lambda expression here base of previous for loop
+    auto test_lbd = [&]()
+    {
+    for (size_t i = 0; i < 100; i++)
+    {
+        std::cout << beta[i];
+        if (beta[i] == '\0')
+        {
+            std::cout << "\n";
+            break;
+        }
+    }
+    };
+
+    test_lbd();
+    std::cout << "test_lbd";
     
     // Output a newline character
     std::cout << "\n";
-    
+    std::cout << "END_1\n";
     // Return 0 to indicate successful execution of the program
     return 0;
 }
